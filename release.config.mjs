89863a0115b4bd1257a3a7f119f07817811042a7
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-08-12 10:52:33 -0500

    Adding semantic-release and empty texture for tests

---
+++ release.config.mjs
@@ -0,0 +1,19 @@
+﻿/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+    branches: ["main", {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
+    "plugins": [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/github",
+            {
+                "assets": [
+                    {path: './zips/**/*.zip'}
+                ]
+            }
+        ]
+    ],
+    tagFormat: "${version}",
+};
